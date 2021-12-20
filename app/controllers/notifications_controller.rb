a20ff3b9344f55be9890e692f53f16d3ef58330c
Koji Kobayashi <koji.kobayashi@livesense.co.jp>
2021-12-20 19:56:35 +0900

    同じ通知をまとめる

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,34 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.order(created_at: :desc)
+    results = current_user.notifications.order(created_at: :desc)
+    num = 0 # 通知列の数
+    user_num = 0
+    additional_message = ""
+    @notifications = []
+    results.each do |result|
+      if result.subject_type == 'Relationship'
+        if @prev_created_at.present?
+          if @prev_created_at <= result.created_at + 5.minutes
+            user_num += 1
+            additional_message = "他" + user_num.to_s + "人"
+          else
+            num += 1
+            user_num = 0
+            additional_message = ""
+          end
+        end
+        @prev_created_at = result.created_at
+        @notifications[num] = {
+          subject_type: result.subject_type,
+          message: result.subject.follower.name + additional_message + "にフォローされました。",
+        }
+      else
+        @notifications << {
+          subject_type: result.subject_type,
+          message: "初回ログインありがとうございます。"
+        }
+      end
+    end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :notifications
 
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -105,14 +106,14 @@ class User < ApplicationRecord
 
   private
 
-    # メールアドレスをすべて小文字にする
-    def downcase_email
-      self.email = email.downcase
-    end
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
 
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
